1a23a7ce9b44600655d6663646b7c2e83308519f
Pritalk <102893888+Pritalk@users.noreply.github.com>
2024-09-28 16:13:26 +0530

    Add terraform provider block in main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+}
 
 resource "local_file" "foo" {
   content  = "I love myself :)"
