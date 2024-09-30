651e081ca9536a22ce82fe30a0c3f0b60ec1f0a8
Pritalk <pritalkendre5087@gmail.com>
2024-09-30 10:41:19 +0530

    Create an ec2 instance for hosting jenkins

--- main.tf
+++ main.tf
@@ -11,3 +11,12 @@ resource "local_file" "foo" {
   content  = "I love myself :)"
   filename = "my_special_file"
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ec2-for-jenkins"
+  }
+}
